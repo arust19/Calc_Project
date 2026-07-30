44fa1228bbdc06a1bc783489fbee5480773062ba
Adam <rustadam9@gmail.com>
2026-07-29 19:00:47 -0500

    Added setup for the 7-Seg Display

--- calc_code/calc_code.ino
+++ calc_code/calc_code.ino
@@ -1,6 +1,10 @@
+#include "LedControl.h"
+
 void setup() {
   // put your setup code here, to run once:
-
+  lc.shutdown(0, false);   // wake up display
+  lc.setIntensity(0, 8);   // brightness 0-15
+  lc.clearDisplay(0);
 }
 
 void loop() {
