0a7932a06a11351149d93291c2578fb20fb7a395
Adam <rustadam9@gmail.com>
2026-07-29 19:33:50 -0500

    Added code structure for calc_project

--- calc_code/calc_code.ino
+++ calc_code/calc_code.ino
@@ -1,7 +1,17 @@
 #include "LedControl.h"
 
+//Map Each button to its corrosponding pin
+const int buttonPin# = 2;  // TODO, Map Each button to the coresponding number on calculator to the Pin on the ATTiny
+
+// LedControl for DIN, clck, cs, number of devices
+LedControl lc = LedControl(4,5,6,1)  // TODO Double Check pin numbers in Altium!
+
 void setup() {
-  // put your setup code here, to run once:
+
+  // Enable Pull up resistors for each button
+  pinMode(buttonPin#, INPUT_PULLUP);
+
+  // Set up for the MAX display driver
   lc.shutdown(0, false);   // wake up display
   lc.setIntensity(0, 8);   // brightness 0-15
   lc.clearDisplay(0);
@@ -9,5 +19,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // Check for button press - Button not pressed = HIGH (pull-UP Resistor)
+  if(digitalRead(buttonPin#) == LOW){
+    //Button Pressed, Add code to display the number/Operator
+  }
+
+  lc.setLed(0,0,0,true) // Set the LED
 
 }
